ec63d6107793a9087170da43815a8a1b42ed44af
Jonathan <jonaenglish64@gmail.com>
2022-07-19 15:51:09 -0500

    Leyendo el input del usuario

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::io;
+use std::io::Write;
 
 fn main() {
-    println!("Hola, Mundo!");
+    println!("Adivina el numerust!");
+
+    print!("Ingresa el numero: ", );
+    io::stdout().flush().unwrap();
+
+    let mut num_elegido = String::new();
+
+    io::stdin()
+        .read_line(&mut num_elegido)
+        .expect("Fallo al leer el prompt");
+    //     let mut guess = String::new();
+
+    // declaramos la variable par ael numero como una nueva instancia del numero
+    println!("El numero que elegiste es {}", num_elegido);
+
+
 }
