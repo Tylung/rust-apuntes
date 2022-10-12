5328885c6c5b022b31d3af83a1325e02474151af
Zack <jonaenglish64@gmail.com>
2022-10-12 15:47:03 -0500

    Add ownership helper

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-mod helpers { 
+mod helpers {
     // pub mod variables;
     // pub mod tipos_datos;
-    pub mod functions;
+    // pub mod functions;
+    pub mod ownership;
     // pub mod macro_rust;
     // pub mod hola;
     // pub mod args_println;
- }
+}
 // creando modulo 📂 para helpers, extrallendo el archivo a ejecutar
-use helpers::functions::main as func;
+use helpers::ownership::main as func;
 
 fn main() {
     func();
