e2165dacf2d9682d5fa49a9b024970a382c60d4f
Jona <jonathanelian64@gmail.com>
2022-12-24 15:40:41 -0700

    Refactor add fn game and optional param

--- src/main.rs
+++ src/main.rs
@@ -8,118 +8,7 @@ use clap::{Command, Arg, ArgMatches};
 fn main() {
     let opts = obt_opciones();
     
-    if opts.contains_id("clear") {
-        clear_scr();
-    }
-
-    if opts.contains_id("infinity"){
-        infinite_game();
-    } else {
-        println!("{}", "Adivina el numerust!".bright_blue());
-    
-        let num_secreto: u8 = rand::thread_rng().gen_range(1..=10);
-        // generamos un numero al azar del 1 al 10
-        println!("{}", "Del 1 al 10".bright_cyan());
-        print!("{}","Ingresa el numero: ".bright_purple() );
-        io::stdout().flush().unwrap();
-        // evitamos que se vaya a otra linea el cursor
-    
-        let mut num_elegido = String::new();
-        // la variable num_elegido es una nueva instancia del struct String, un nuevo string
-    
-        io::stdin()
-            .read_line(&mut num_elegido)
-            // leemos la linea del prompt
-            .expect("Fallo al leer el prompt" );
-        
-        let num_elegido: u8 = match num_elegido.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("{}", "No ingresaste un numero valido".red());
-                return ;
-            }
-        };
-        // shadow variable para convertirlo en numero y poder compararlo con el numero secreto
-        // y utilizamos la expresion match para que cuando no reciba un numero simpremente continue el programa
-    
-        println!("{} {}", "El numero que elegiste es".dimmed() , num_elegido.yellow() );
-    
-        match num_elegido.cmp( &num_secreto) {
-            Ordering::Less => println!("{}, el numero era {}", "Muy bajo".red(), num_secreto.bright_red()),
-            Ordering::Greater => println!("{}, el numero era {}", "Muy alto".red(), num_secreto.bright_red()),
-            Ordering::Equal =>  println!("{} el numero era {}", "Has Acertado :D".green(), num_secreto.bright_green())
-        }
-    }
-
-    
-
-
-}
-
-fn infinite_game(){
-    loop {
-        let mut resp = String::new();
-        clear_scr();
-        println!("{}", "Adivina el numerust!".bright_blue());
-        
-        let num_secreto: u8 = rand::thread_rng().gen_range(1..=10);
-        // generamos un numero al azar del 1 al 10
-        println!("{}", "Del 1 al 10".bright_cyan());
-        print!("{}","Ingresa el numero: ".bright_purple() );
-        io::stdout().flush().unwrap();
-        // evitamos que se vaya a otra linea el cursor
-
-        let mut num_elegido = String::new();
-        // la variable num_elegido es una nueva instancia del struct String, un nuevo string
-
-        io::stdin()
-            .read_line(&mut num_elegido)
-            // leemos la linea del prompt
-            .expect("Fallo al leer el prompt" );
-        
-        let num_elegido: u8 = match num_elegido.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        // shadow variable para convertirlo en numero y poder compararlo con el numero secreto
-        // y utilizamos la expresion match para que cuando no reciba un numero simpremente continue el programa
-
-        println!("{} {}", "El numero que elegiste es".dimmed() , num_elegido.yellow() );
-
-        match num_elegido.cmp( &num_secreto) {
-            Ordering::Less => println!("{}, el numero era {}", "Muy bajo".red(), num_secreto.bright_red()),
-            Ordering::Greater => println!("{}, el numero era {}", "Muy alto".red(), num_secreto.bright_red()),
-            Ordering::Equal => {
-                println!("{} el numero era {}", "Has Acertado :D".green(), num_secreto.bright_green());
-                break;
-            }
-        }
-
-        print!("{}{}","\nDesea jugar otravez?".bright_green(), "(s/N) ".cyan().dimmed());
-        io::stdout().flush().unwrap();
-
-        io::stdin()
-            .read_line( &mut resp)
-            .expect("No es una entrada valida");
-
-        resp = resp.trim().to_ascii_lowercase();
-
-        if resp.chars().count() == 1 && resp.contains("s") {
-            println!("{}", "\nCreando nuevo juego...".bright_cyan());
-            io::stdout().flush().unwrap();
-            std::thread::sleep(Duration::from_millis(500));
-        } else {
-            break;
-        } 
-        
-
-    }
-}
-
-fn clear_scr() {
-    print!("{esc}[2J {esc}[1;1H", esc = 27 as char);
-    // funciona en linux y windows! 
-    // limpia la pantalla y coloca el cursor en la primera columna y primera fila
+	init_game(opts);
 }
 
 
@@ -145,4 +34,107 @@ fn obt_opciones() -> ArgMatches {
         .get_matches();
     
     return opts;
+}
+
+fn init_game(opts: ArgMatches) {
+	if opts.contains_id("clear") {
+        clear_scr();
+    }
+
+    if opts.contains_id("infinity"){
+        infinite_game();
+		return;
+    }
+
+	game(None);
+
+}
+
+fn game<T: Into<Option<u8>>>(num: T) -> bool {
+
+	let num_secreto: u8;
+
+	if let Some(num) = num.into() {
+		num_secreto = num;
+	} else {
+		num_secreto = rand::thread_rng().gen_range(1..=10);
+	}
+	
+    println!("{}", "Adivina el numerust!".bright_blue());
+
+	let mut is_winner: bool = false;
+    // generamos un numero al azar del 1 al 10
+    println!("{}", "Del 1 al 10".bright_cyan());
+    print!("{}","Ingresa el numero: ".bright_purple() );
+    io::stdout().flush().expect("Error al flush stdout");
+
+    let mut num_elegido = String::new();
+    // la variable num_elegido es una nueva instancia del struct String, un nuevo string
+
+    io::stdin()
+        .read_line(&mut num_elegido)
+        // leemos la linea del prompt
+        .expect("Fallo al leer el prompt" );
+    
+    let num_elegido: u8 = match num_elegido.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("{}", "No ingresaste un numero valido".red());
+            return is_winner;
+        }
+    };
+    // shadow variable para convertirlo en numero y poder compararlo con el numero secreto
+    // y utilizamos la expresion match para que cuando no reciba un numero simpremente continue el programa
+
+    println!("{} {}", "El numero que elegiste es".dimmed() , num_elegido.yellow() );
+
+    match num_elegido.cmp( &num_secreto) {
+        Ordering::Less => println!("{}, el numero era {}", "Muy bajo".red(), num_secreto.bright_red()),
+        Ordering::Greater => println!("{}, el numero era {}", "Muy alto".red(), num_secreto.bright_red()),
+        Ordering::Equal => {
+			println!("{} el numero era {}", "Has Acertado :D".green(), num_secreto.bright_green());
+			is_winner = true
+		}
+    }
+
+	is_winner
+}
+
+
+fn infinite_game(){
+    loop {
+        let mut resp = String::new();
+        clear_scr();
+
+		let is_winner = game(None);
+
+		if is_winner {
+			return;
+		}
+
+        print!("{}{}","\nDesea jugar otravez?".bright_green(), "(s/N) ".cyan().dimmed());
+        io::stdout().flush().expect("Error al flush stdout");
+
+        io::stdin()
+            .read_line( &mut resp)
+            .expect("No es una entrada valida");
+
+        resp = resp.trim().to_ascii_lowercase();
+
+        if resp.chars().count() == 1 && resp.contains("s") {
+            println!("{}", "\nCreando nuevo juego...".bright_cyan());
+            io::stdout().flush().expect("Error en flush stdout");
+            std::thread::sleep(Duration::from_millis(200));
+        } else {
+            break;
+        } 
+        
+
+    }
+}
+
+fn clear_scr() {
+    print!("{esc}[2J {esc}[1;1H", esc = 27 as char);
+    // funciona en linux y windows! 
+    // limpia la pantalla y coloca el cursor en la primera columna y primera fila
 }
