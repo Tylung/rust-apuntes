0c8e6c7081df37a9bba70c8bb511c1828d19fc74
jona <jonaenglish64@gmail.com>
2022-12-27 04:48:56 -0600

    Move ownership topic files to specific folder

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-mod basics {
-    // pub mod args_println;
-    // pub mod functions;
-    // pub mod hola;
-    // pub mod macro_rust;
-    pub mod ownership;
-    pub mod borrowing;
-    // pub mod tipos_datos;
-    // pub mod variables;
+mod ownership {
+  pub mod intro;
+  pub mod borrowing;
 }
+
 // creando modulo 📂 para basics, extrallendo el archivo a ejecutar
-use basics::ownership::main as ownership;
-use basics::borrowing::main as borrowing;
+use ownership::intro::main as ownership;
+use ownership::borrowing::main as borrowing;
 
 fn main() {
     ownership();
